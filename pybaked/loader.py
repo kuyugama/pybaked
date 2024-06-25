678c8819abd2dada6f01e8257578746fb481a3d2
kuyugama <mail.kuyugama@gmail.com>
2024-06-25 08:32:29 +0300

    Refactor: added comments and removed unclear and unnecessary logic

--- pybaked/loader.py
+++ pybaked/loader.py
@@ -7,103 +7,120 @@ from pathlib import Path
 from pybaked import BakedReader
 
 
-def execute(source: str | bytes, module: types.ModuleType):
+def execute_module(source: str | bytes, module: types.ModuleType):
     code = compile(source, module.__file__, "exec")
     exec(code, module.__dict__)
 
 
 class BakedPathFinder(MetaPathFinder):
     def find_spec(self, fullname, path, target=...):
+        # Define path entries in which finder will search baked packages
         entries = sys.path
+        # Extend entries with package path
         if path is not None:
             entries.extend(path)
-        # Search in default places
+
         for path_entry in entries:
-            path_entry = Path(path_entry)
+            # Using more convenient interface for working with path
+            baked_package = Path(path_entry)
 
-            package_name = fullname.rsplit(".", 1)
-
-            if len(package_name) == 1:
-                package_name = fullname
-            else:
-                package_name, _ = package_name
-
-            baked_package = Path(f"{path_entry}")
-
-            inner_module_name = fullname
-
+            # Find baked package inside path entry following
+            # the module name parts
             parts = fullname.split(".")
-
             for i, part in enumerate(parts):
                 baked_package /= part
 
-                if baked_package.with_suffix(
-                    ".py.baked"
-                ).exists():
-                    baked_package = baked_package.with_suffix(
-                        ".py.baked"
-                    )
+                # If this is baked package - use it and break loop
+                with_suffix = baked_package.with_suffix(".py.baked")
+                if with_suffix.exists():
+                    baked_package = with_suffix
+
+                    # Define inner module name relative to
                     inner_module_name = ".".join(parts[i:])
                     break
+            else:
+                # If package was not found - skip this path entry
+                continue
 
-            if baked_package.exists():
-                reader = BakedReader(baked_package)
-                location = str(baked_package.joinpath(*inner_module_name.split(".")[1:]))
+            # Defining baked package reader to search module inside
+            reader = BakedReader(baked_package)
 
-                print(inner_module_name, reader.modules_dict)
+            # Define module location
+            location = str(
+                baked_package.joinpath(*inner_module_name.split(".")[1:])
+            )
 
-                if inner_module_name in reader.modules_dict:
-                    location += ".py"
-                elif inner_module_name not in reader.packages:
-                    continue
+            # If it is a normal module - add .py suffix (just in case)
+            if inner_module_name in reader.modules_dict:
+                location += ".py"
 
-                return importlib.util.spec_from_file_location(
-                    inner_module_name,
-                    location,
-                    loader=BakedLoader(reader, package_name),
-                )
+            # If module not found and it is not a package
+            # inside with this name - skip this baked package
+            elif inner_module_name not in reader.packages:
+                continue
+
+            # Build spec for module
+            return importlib.util.spec_from_file_location(
+                fullname,
+                location,
+                loader=BakedLoader(reader, inner_module_name),
+            )
         return None
 
 
 class BakedLoader(Loader):
-    def __init__(self, reader: BakedReader, package_name: str):
+    def __init__(self, reader: BakedReader, inner_module_name: str):
         self.reader = reader
-        self.package_name = package_name
+        self.inner_module_name = inner_module_name
 
     def create_module(self, spec):
         return types.ModuleType(spec.name)
 
     def exec_module(self, module):
-        module_package = module.__name__ in self.reader.packages
+        # Define what module will be read from reader.
+        # If module is the package then it may be
+        # package_name + .__init__ if exists module with that name
+        module_name = self.inner_module_name
 
-        module.__package__ = module.__name__.rsplit(".", 1)[0]
-
-        formatted_name = ".".join(module.__name__.split("."))
-
-        if module_package:
+        # If module is the package - package must be equal to module name
+        if module_name in self.reader.packages:
             module.__package__ = module.__name__
 
-            if formatted_name + ".__init__" in self.reader.modules_dict:
-                formatted_name += ".__init__"
+            # If package has __init__ module - load it
+            if module_name + ".__init__" in self.reader.modules_dict:
+                module_name += ".__init__"
+        else:
+            module.__package__ = module.__name__.rsplit(".", 1)[0]
 
-        formatted_name = formatted_name.strip(".")
+        # Location module inside the baked package
+        # (first element is the name of the package)
+        module_location_inside = module_name.split(".")[1:]
 
+        # Define path of the module file
         module.__file__ = (
-                str(self.reader.path / Path(*formatted_name.split(".")[1:])) + ".py"
+            str(self.reader.path / Path(*module_location_inside)) + ".py"
         )
 
+        # Define package module resolution path
         module.__path__ = [self.reader.path]
-        if formatted_name not in self.reader.modules_dict:
+
+        # If module not found in the package - leaving from loader
+        if module_name not in self.reader.modules_dict:
             return
 
-        source_offset = self.reader.modules_dict[formatted_name]
+        # Offset of the module source in the baked package file
+        source_offset = self.reader.modules_dict[module_name]
 
+        # Source of the module
         source = self.reader.read_specific(source_offset)
 
-        execute(source, module)
+        # Compile and execute module code
+        execute_module(source, module)
 
-        if self.reader.path.name.split(".")[0] == module.__package__:
-            module.__metadata__ = self.reader.metadata
+        # Install metadata read by reader to all root modules of the baked package
+        # Use it to recognize baked modules in code
+        if module_name.split(".")[0] == module.__package__:
+            module.__baked_metadata__ = self.reader.metadata
 
 
 def init():
